cad2f4d74d116b53df092ff35d65e1c8f21de88a
TheEnderOfficial <70067741+TheEnderOfficial@users.noreply.github.com>
2021-03-19 21:31:40 +0300

    Many changes

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from mysql.connector import connect
 import os
 import dotenv
 from flask_cors import CORS
+import uuid
 
 dotenv.load_dotenv()
 config = {
@@ -18,11 +19,17 @@ cursor = mysqldb.cursor()
 app = flask.Flask("AccesControlSystem", static_folder='', template_folder='frontend')
 CORS(app)
 
-@app.route('/login', methods=["GET", "POST"])
-@app.route('/login.html', methods=["GET", "POST"])
+@app.route('/', methods=["GET", "POST"])
+@app.route('/index', methods=["GET", "POST"])
+@app.route('/indez.html', methods=["GET", "POST"])
 def index():
-    pass
-
+    if not flask.request.cookies.get('token'):
+        return flask.redirect('login.html')
+    cursor.execute("SELECT * FROM users WHERE token", (flask.request.cookies.get('token')))
+    user = cursor.fetchone()
+    if not user:
+        return flask.redirect('login.html')
+    return flask.render_template('index.html', user=user)
 @app.route('/login', methods=["GET", "POST"])
 @app.route('/login.html', methods=["GET", "POST"])
 def login():
@@ -30,7 +37,12 @@ def login():
     if flask.request.method == "POST":
         cursor.execute("SELECT * FROM users WHERE login = %s AND password = %s", (flask.request.form.get('login'), flask.request.form.get('pass')))
         user = cursor.fetchone()
-        print(user)
+        user_token = uuid.uuid1()
+        cursor.execute("UPDATE users SET token = %s WHERE id = %s", (user_token.hex, user[0]))
+        mysqldb.commit()
+        res = flask.redirect('/')
+        res.set_cookie('token', bytes(user_token.hex.encode()), max_age=60*60*24*365*5)
+        return res
     return flask.render_template("login.html")
 
 
