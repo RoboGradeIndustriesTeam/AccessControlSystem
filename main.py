ce41a956dc38e9822fc55ba36e3568459dd6b440
TheEnderOfficial <70067741+TheEnderOfficial@users.noreply.github.com>
2021-03-23 18:49:06 +0300

    Fix 3

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ def user():
         users2 = tmp2.securityUsers
         users = []
         for i in users2:
-            users.append(GetUserByID(users[1]))
+            users.append(GetUserByID(i[1]))
         userAnalitycs = tmp2.fetch(mysqldb, user.id)
         if flask.request.method == "POST":
             ftyp = flask.request.form.get("form-type")
